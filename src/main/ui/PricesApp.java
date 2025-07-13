cfaae26a6c9ca2961c96acf0fbac108414a8f259
jdonner <jdonner@students.cs.ubc.ca>
2025-07-13 09:00:08 -0700

    grocery + category listing + price handling. broken grocery editing functionality

--- src/main/ui/PricesApp.java
+++ src/main/ui/PricesApp.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 public class PricesApp {
     private Scanner in;
     private ArrayList<Grocery> groceries;
+    private ArrayList<Category> categories;
 
     // EFFECTS: runs the prices application
     public PricesApp() {
@@ -59,6 +60,10 @@ public class PricesApp {
             comAddGrocery();
         } else if (command.equals("list")) {
             comListGroceries();
+        } else if (command.equals("listc")) {
+            comListCategory();
+        } else if (command.equals("listc g")) {
+            comListCategoryGroceries();
         } else {
             System.out.println("Invalid command.");
         }
@@ -70,6 +75,7 @@ public class PricesApp {
      */
     private void init() {
         groceries = new ArrayList<Grocery>();
+        categories = new ArrayList<Category>();
         in = new Scanner(System.in);
         in.useDelimiter("\\R");
     }
@@ -84,8 +90,31 @@ public class PricesApp {
         System.out.println("\tcomp -> compare a grocery's price to a given price");
         System.out.println("\tc -> create a new category");
         System.out.println("\tadd -> add a grocery to a category");
-        System.out.println("\tlist -> list all groceries (and their prices) in a category");
+        System.out.println("\tlist -> list all groceries (and their prices)");
+        System.out.println("\tlistc -> list all categories");
+        System.out.println("\tlistc g -> list all groceries (and their prices) in a category");
         System.out.println("\tq -> quit");
+    }
+
+    /*
+     * EFFECTS: continues to prompt user until user returns a string that can be
+     * converted to a BigDecimal price, then returns price as a BigDecimal
+     */
+    private BigDecimal priceHandling(String price) {
+        try {
+            BigDecimal convert = new BigDecimal(price);
+            if (convert.scale() == 2) {
+                return convert;
+            } else {
+                System.out.print("Please enter a grocery price with exactly two digits after the decimal: ");
+                price = in.next();
+                return priceHandling(price);
+            }
+        } catch (NumberFormatException e) {
+            System.out.print("Please enter a valid grocery price: ");
+            price = in.next();
+            return priceHandling(price);
+        }
     }
 
     /*
@@ -93,27 +122,11 @@ public class PricesApp {
      * EFFECTS: goes through new grocery creation process
      */
     private void comNewGrocery() {
-        System.out.print("Enter name of grocery: ");
+        System.out.print("\nEnter name of grocery: ");
         String name = in.next();
         System.out.print("Enter price of grocery: ");
         String price = in.next();
-        Boolean r = true;
-        while (r) {
-            try {
-                while (r) {
-                    BigDecimal priceConverted = new BigDecimal(price);
-                    if (priceConverted.scale() == 2) {
-                        groceries.add(new Grocery(name, new BigDecimal(price)));
-                        r = false;
-                        break;}
-                    System.out.print("Please enter a grocery price with exactly two digits after the decimal: ");
-                    price = in.next();
-                }
-            } catch (NumberFormatException e) {
-                System.out.print("Please enter a valid grocery price: ");
-                price = in.next();
-            }
-        }
+        groceries.add(new Grocery(name, priceHandling(price)));
         System.out.println("Successfully added \"" + groceries.get(groceries.size() - 1).getName() + "\" :D");
     }
 
@@ -122,34 +135,80 @@ public class PricesApp {
      * EFFECTS: goes through grocery editing process
      */
     private void comEditGrocery() {
-
+        System.out.print("\nEnter name of grocery to edit: ");
+        String name = in.next();
+        Boolean r = true;
+        Grocery toEdit = null;
+        while (r) {
+            for (Grocery g : groceries) {
+                if (name == g.getName()) {
+                    System.out.println("made it");
+                    toEdit = g;
+                    break;
+                } else {
+                    System.out.println("sad");
+                }
+            }
+            System.out.print("Grocery not found, please try again: ");
+            name = in.next();
+        }
+        System.out.print("Enter new price for " + toEdit.getName() + ": ");
+        String price = in.next();
+        toEdit.editPrice(priceHandling(price));
     }
 
     /*
-     * EFFECTS: goes through grocery editing process
+     * EFFECTS: goes through grocery price comparison process
      */
     private void comCompareGrocery() {
 
     }
 
     /*
-     * EFFECTS: goes through grocery editing process
+     * EFFECTS: goes through new category creation process
      */
     private void comNewCategory() {
 
     }
 
     /*
-     * EFFECTS: goes through grocery editing process
+     * EFFECTS: goes through adding grocery to category process
      */
     private void comAddGrocery() {
 
     }
 
     /*
-     * EFFECTS: goes through grocery editing process
+     * EFFECTS: lists every grocery and price
      */
     private void comListGroceries() {
+        if (groceries.isEmpty()) {
+            System.out.println("No groceries :/");
+        } else {
+            for (Grocery g : groceries) {
+                System.out.println(g.toString());
+            }
+        }
+    }
+
+    /*
+     * EFFECTS: lists every category
+     */
+    private void comListCategory() {
+        if (categories.isEmpty()) {
+            System.out.println("No categories :/");
+        } else {
+            for (Category c : categories) {
+                System.out.println(c.getName());
+            }
+        }
+    }
+
+    /*
+     * EFFECTS: goes through selecting a category process then lists all groceries
+     * and prices in category
+     */
+    private void comListCategoryGroceries() {
 
     }
 }
